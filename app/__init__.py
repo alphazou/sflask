edf46111b4e837d3df3d9b31db2dfdb4f86b1498
alphazou <alphazou@qq.com>
2017-05-23 08:33:15 +0800

    5月23日，数据库迁移

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_moment import Moment
 from flask_script import Manager
 
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_migrate import Migrate, MigrateCommand
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -12,7 +12,8 @@ moment = Moment(app)
 app.config.from_object('config')
 manager = Manager(app)
 db = SQLAlchemy(app)
-
+migrage = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 
 from app import views
 
